bb51afc3d8f5b066be21a5e3a6cadbbd6dfe84fd
miller777 <miller777@mail.ru>
2016-08-09 03:04:28 +0300

    Прикрутил к службе вывод всплывающих сообщений.

--- Joke/app/src/main/java/com/hfad/joke/DelayedMessageService.java
+++ Joke/app/src/main/java/com/hfad/joke/DelayedMessageService.java
@@ -2,15 +2,24 @@ package com.hfad.joke;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.Toast;
 
 
 public class DelayedMessageService extends IntentService {
 
     public static final String EXTRA_MESSAGE = "message";
+    private Handler handler;
 
     public DelayedMessageService() {
         super("DelayedMessageService");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        handler = new Handler();
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
@@ -28,7 +37,13 @@ public class DelayedMessageService extends IntentService {
 
     private void showText(final String text) {
 
-        Log.v("DelayedMessageService", "The message is: " + text);
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
 
